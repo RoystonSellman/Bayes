ad2bd0eb83b1e45842afe6d33b4513785bd15d1e
Royston Sellman <roystone54@gmail.com>
2014-02-17 16:46:29 +0000

    Tidied up version for sending to Jim Stone

--- Bayes/Regression.py
+++ Bayes/Regression.py
@@ -17,23 +17,22 @@ import matplotlib.pyplot as plt
 import statsmodels.regression.linear_model as sm
 
 s=6 # set random number seed.
-#rand('seed',s);    randn('seed',s);
+                                        #   In MatLabversion: rand('seed',s);    randn('seed',s);
 
 # Make noisy data with slope m and intercept c.
 # Define 10 salary values for horizontal axis.
-s = arange(1,12)                        # s = 1:1:11
-#s = s.reshape(11,1)                    # s=s';
+s = arange(1,12)                        #   In MatLabversion: s = 1:1:11
+#s = s.reshape(11,1)                    #   In MatLabversion: s=s';
 
 # Set true values of slope m and intercept c.
 m = 0.5
 c = 3
 # Set standard deviation of each measured height.
-sds = 2*arange(1,12)/10.0 #sds = 2*[1:11]’/10; %rand(size(s))*2;
-sds = sds * arange(1,12)/10.0 #sds = sds .* [1:11]'/10;
-#sds = sds.reshape(11,1)
+sds = 2*arange(1,12)/10.0               #   In MatLabversion: sds = 2*[1:11]’/10; %rand(size(s))*2;
+sds = sds * arange(1,12)/10.0           #   In MatLabversion: sds = sds .* [1:11]'/10;
 
 # Use sds to make vector of noise.
-eta = random.randn(len(s)) * sds # eta = randn(size(s)).*sds;
+eta = random.randn(len(s)) * sds        #   In MatLabversion: eta = randn(size(s)).*sds;
 # Pretend some data points are extreme.
 eta[7]=-1
 eta[8]=-3
@@ -45,26 +44,25 @@ x = m*s + c + eta
 # Weighted Least Squares regression. We could find the solution using the smallest value in the Farray below also.
 # Find weightings w (discount) for each data point.
 vars0 = sds ** 2
-w=1/vars0 # w=1./vars0
+w=1/vars0                               #   In MatLabversion: w=1./vars0
 #w=ones(size(w)) # un-comment this line to get solution based on uniform noise terms.
-ss = sm.add_constant(s) #ss=[ones(size(s)) s] # prepend column of ones so that solution includes intercept term.
-model = sm.WLS(ss, x, weights=w) #[params,stdx,mse,S] =lscov(ss,x,w)
+ss = sm.add_constant(s)                 #   In MatLabversion: ss=[ones(size(s)) s] # prepend column of ones so that solution includes intercept term.
+model = sm.WLS(ss, x, weights=w)        #   In MatLabversion: [params,stdx,mse,S] =lscov(ss,x,w)
 results = model.fit()
 mest2, cest2 = results.params[0]
 cest2 = cest2 + c # +c to compensate for apparent incorrect intercept returned from WLS
 ########
-xest2 = mest2 * s + cest2 #xest2 = mest2.*s + cest2;
+xest2 = mest2 * s + cest2               #   In MatLabversion: xest2 = mest2.*s + cest2;
 c0 = cest2;
 m0 = mest2;
 
 # Plot fitted line xest (=xhat in text) and data points.
 fig1 = plt.figure() # ; clf;
-plt.plot(s,x, 'k*', s, xest2, 'k') #, 'LineWidth',2, 'MarkerSize',10)
-# set(gca,'Linewidth',2); set(gca,'FontSize',20);
+plt.plot(s,x, 'k*', s, xest2, 'k')
 plt.xlabel('Salary, ' + r'$s$' + ' (groats)'); # use trick to get italic font
 plt.ylabel('Height, ' + r'$x$' + ' (feet)');
-plt.xlim((0,12)) # set(gca,'XLim',[0 12],'FontName','Ariel');
-plt.ylim((0,9)) # set(gca,'YLim',[0 9],'FontName','Ariel');
+plt.xlim((0,12))
+plt.ylim((0,9))
 # Plot sd  from each data point
 for i in range(0,11):
     x1 = x[i]-sds[i]
@@ -74,7 +72,7 @@ for i in range(0,11):
     ss = (s1, s2)
     xx = (x1, x2)
     # hold on;
-    plt.plot(ss,xx,'k', linewidth=2) #,'LineWidth',2) # hold off;
+    plt.plot(ss,xx,'k', linewidth=2)
 plt.show()
 
 
@@ -91,16 +89,16 @@ minc = (mmax-mmin)/100
 cinc = (cmax-cmin)/100
 
 Fs = []
-#ms = mmin:minc:mmax;
+#  In MatLabversion: ms = mmin:minc:mmax;
 ms = linspace(mmin, mmax, 100) # minc not used as linspace takes number of values to be generated
 nm = len(ms)
-#cs = cmin:cinc:cmax);
+#  In MatLabversion: cs = cmin:cinc:cmax);
 cs = linspace(cmin, cmax, 100) # cinc not used as linspace takes number of values to be generated
 nc = len(cs)
 
 Farray = zeros((nm,nc))
-for m1 in arange(0,nm): #= 1:nm
-    for c1 in arange(0, nc): # = 1:nc
+for m1 in arange(0,nm):                 #  In MatLabversion: 1:nm
+    for c1 in arange(0, nc):            #  In MatLabversion: 1:nc
         mval=ms[m1]
         cval=cs[c1]
         y1 = mval*s + cval
@@ -110,15 +108,15 @@ for m1 in arange(0,nm): #= 1:nm
 
 fig2 = plt.figure()
 Z1 = Farray.T
-zmin = amin(Z1) # min(Z1(:));
-zmax = amax(Z1) # max(Z1(:));
+zmin = amin(Z1)                         #  In MatLabversion: min(Z1(:));
+zmax = amax(Z1)                         #  In MatLabversion: max(Z1(:));
 zrange = zmax - zmin # cannot use variable name 'range' in python
-v = arange(zmin, zmax, zrange / 10) # =zmin:range/10:zmax;
+v = arange(zmin, zmax, zrange / 10)     #  In MatLabversion: min:range/10:zmax;
 # Adjust spacing of contour lines.
 v = arange(0, 9 ,0.5)
 v=exp(v)
 v=v*zrange/max(v);
-X, Y = np.meshgrid(ms,cs) #;[X Y]=meshgrid(ms,cs);
+X, Y = np.meshgrid(ms,cs)               #  In MatLabversion: [X Y]=meshgrid(ms,cs);
 plt.xlabel('Slope, ' + r'$m$') # use trick to get italic font
 plt.ylabel('Intercept, ' + r'$c$');
 plt.contour(X, Y, Z1, v)
